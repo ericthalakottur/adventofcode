4bf06243b667df6ba2fb05bc230f0c1a4f525499
Eric Thalakottur <184715709+ericthalakottur@users.noreply.github.com>
2025-03-05 23:25:44 +0530

    Day 1 Solution 2 in Rust

--- 2024/1/solve.rs
+++ 2024/1/solve.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::str::FromStr;
@@ -27,4 +28,21 @@ fn main() {
         answer1 += (v1[i] - v2[i]).abs();
     }
     println!("Answer 1: {answer1}");
+
+    let mut answer2 = 0;
+    let mut counter: HashMap<i32, i32> = HashMap::new();
+    let mut i = 0;
+    while i < v2.len() {
+        let mut count = 1;
+        while i + 1 < v2.len() && v2[i] == v2[i + 1] {
+            count += 1;
+            i += 1
+        }
+        counter.insert(v2[i], count);
+        i += 1;
+    }
+    for (_, value) in v1.iter().enumerate() {
+        answer2 += *value * *counter.entry(*value).or_default();
+    }
+    println!("Answer 2: {answer2}");
 }
